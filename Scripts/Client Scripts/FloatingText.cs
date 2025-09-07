d049293ece01957e458431d33389aca556ea92d4
Matthew Nock <thematt.nock@gmail.com>
2025-09-07 19:01:11 +0100

    moved floating text to make it bounce a bit more

--- Scripts/Client Scripts/FloatingText.cs
+++ Scripts/Client Scripts/FloatingText.cs
@@ -11,7 +11,7 @@ public partial class FloatingText : Marker2D
     {
         var tween = CreateTween();
         var rng = new RandomNumberGenerator();
-        var SideMovement = rng.RandiRange(60, 100) - 40;
+        var SideMovement = rng.RandiRange(-40, 40);
         Vel = new Vector2(SideMovement, 25);
         tween.TweenProperty(this, "scale", new Vector2(2, 2), 0.5f).SetTrans(Tween.TransitionType.Linear).SetEase(Tween.EaseType.Out);
         tween.TweenProperty(this, "scale", new Vector2(0.5f, 0.5f), 0.5f).SetTrans(Tween.TransitionType.Linear).SetEase(Tween.EaseType.Out);
