8cc312b3a56eee5bd8f4ec795718c98fda14769f
Matthew Nock <thematt.nock@gmail.com>
2025-09-13 11:34:02 +0100

    Simple Equipment UI, started not done yet

---
+++ EquipmentUI.cs
@@ -0,0 +1,80 @@
+using Godot;
+using System;
+
+public partial class EquipmentUI : Panel
+{
+    [Export] public Area2D area;
+    public bool mouseIn;
+    public bool mouseClick;
+    public EquipmentSlotUI selectedSlot;
+    public GridContainer grid;
+    public Panel TopUI;
+    public override void _Ready()
+    {
+        grid = GetParent<GridContainer>();
+        TopUI = GetParent().GetParent().GetParent<Panel>();
+        area.MouseEntered += () =>
+        {
+            mouseIn = true;
+        };
+        area.MouseExited += () =>
+        {
+            mouseIn = false;
+        };
+        area.AreaEntered += OnEquipSlotEnter;
+        area.AreaExited += OnEquipSlotExit;
+
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton MB)
+        {
+            if (!MB.Pressed)
+            {
+                mouseClick = false;
+                if (selectedSlot != null)
+                {
+                    GlobalPosition = selectedSlot.GlobalPosition;
+                }
+                else
+                {
+                    grid.CallDeferred("add_child", this);
+                    GetParent().RemoveChild(this);
+                }
+            }
+            if (MB.Pressed && mouseIn)
+            {
+                TopUI.CallDeferred("add_child", this);
+                GetParent().RemoveChild(this);
+                mouseClick = true;
+            }
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        if (mouseClick)
+        {
+            GlobalPosition = GetGlobalMousePosition() + new Vector2(-45, -45);
+        }
+        
+
+    }
+
+    public void OnEquipSlotEnter(Node2D body)
+    {
+        if (body.GetParent() is EquipmentSlotUI)
+        {
+            selectedSlot = body.GetParent<EquipmentSlotUI>();
+        }
+    }
+
+    public void OnEquipSlotExit(Node2D body)
+    {
+        if (selectedSlot == body.GetParent())
+        {
+            selectedSlot = null;
+        }
+    }
+}
