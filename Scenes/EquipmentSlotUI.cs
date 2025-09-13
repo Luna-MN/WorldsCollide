d4df505dfc01ec09dbe91c414212d147a27deb84
Matthew Nock <thematt.nock@gmail.com>
2025-09-13 18:37:18 +0100

    Added equipment slot icons for the armour

--- Scenes/EquipmentSlotUI.cs
+++ Scenes/EquipmentSlotUI.cs
@@ -1,6 +1,16 @@
 using Godot;
 using System;
-
+[Tool]
 public partial class EquipmentSlotUI : Panel
 {
+    [Export]
+    public Sprite2D IconSprite;
+
+    [Export]
+    public Texture2D Icon
+    {
+        get => IconSprite.Texture;
+        set => IconSprite.Texture = value;
+    }
+    
 }
