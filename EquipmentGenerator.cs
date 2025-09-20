8150735be0fe0d5b06d43317bfe4651159474338
Matthew Nock <thematt.nock@gmail.com>
2025-09-20 10:14:20 +0100

    Work on rarity generation

--- EquipmentGenerator.cs
+++ EquipmentGenerator.cs
@@ -9,7 +9,7 @@ public partial class EquipmentGenerator : Node2D
     public int Level;
     public float Prestige;
     public List<int> CharacterIds;
-    
+    public float GeneratePercentage = 0.85f;
     public enum Rarity
     {
         Common,
@@ -20,9 +20,51 @@ public partial class EquipmentGenerator : Node2D
         Legendary,
         Godly
     }
-    // I need a list of Ids to generate items for
+    private RandomNumberGenerator rng = new RandomNumberGenerator();
+    public override void _Ready()
+    {
+        rng.Randomize();
+    }
+
     public override void _Process(double delta)
     {
-        
+        foreach (var Id in CharacterIds)
+        {
+            var RandPercentage = (float)Mathf.Snapped(rng.RandfRange(0, 1), 0.0001);
+            
+            if(RandPercentage >= GeneratePercentage)
+            {
+                RandPercentage = rng.RandfRange(0, 1);
+                var ItemRarity = RaritySwitch(RandPercentage);
+                
+            }
+            else
+            {
+                QueueFree();
+            }
+        }
+    }
+
+    public Rarity RaritySwitch(float RarityPercentage)
+    {
+        switch (RarityPercentage)
+        {
+            case < 0.45f:
+                return Rarity.Common;
+            case < 0.75f:
+                return Rarity.Uncommon;
+            case < 0.9f:
+                return Rarity.Rare;
+            case < 0.965f:
+                return Rarity.Epic;
+            case < 0.994f:
+                return Rarity.Pristine;
+            case < 0.999f:
+                return Rarity.Legendary;
+            case < 1f:
+                return Rarity.Godly;
+            default:
+                return Rarity.Common;
+        }
     }
 }
