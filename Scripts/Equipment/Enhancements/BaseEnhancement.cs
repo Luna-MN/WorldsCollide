ca28c475b74bb5c0f5271329021206ab8a4e84a3
Matthew Nock <thematt.nock@gmail.com>
2025-09-20 19:34:27 +0100

    setting up enhancments

--- Scripts/Equipment/Enhancements/BaseEnhancement.cs
+++ Scripts/Equipment/Enhancements/BaseEnhancement.cs
@@ -1,19 +1,29 @@
 using Godot;
 using System;
-
+[Tool]
 [GlobalClass]
 public partial class BaseEnhancement : Resource
 {
-    [Export]
+    [Export(PropertyHint.MultilineText, "if you put {Value} it will replace with the value")]
     public string EnhancmentText;
     [Export]
     public Flags.AbilityFlags EnhancmentFlags;
     [Export(PropertyHint.Range, "1, 7, 1")]
     public int MinEnhancmentLevel;
-    
+    [ExportGroup("Value Based")]
+    [Export(PropertyHint.GroupEnable)]
+    public bool ValueBased;
+    public float Value = 10;
+    [Export] public float MinValue;
+    [Export] public float MaxValue;
     public virtual void Enhance(Character character)
     {
         //add what you want it to do here
         // so like character.OnHit += something
+    }
+
+    public string GetDescription()
+    {
+        return EnhancmentText.Replace("{Value}", Value.ToString());
     }
 }
