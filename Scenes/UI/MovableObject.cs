1709c84ece532b7b275e6c3e6331d7555f9a4d18
Matthew Nock <thematt.nock@gmail.com>
2025-10-10 19:23:22 +0100

    Added resizing functionality to `MovableObject` for adjustable UI panels; updated `Test UI.tscn` to utilize the new `MovableObject.tscn` instance.

--- Scenes/UI/MovableObject.cs
+++ Scenes/UI/MovableObject.cs
@@ -1,42 +1,63 @@
+
 using Godot;
 using System;
+
 [GlobalClass]
 public partial class MovableObject : Panel
 {
     [Export]
-    public Area2D MoveArea;
-    public bool mouseIn;
+    public Control MoveArea;
+    [Export]
+    public Control ScaleArea;
+    [Export]
+    public Vector2 MinSize = new Vector2(50, 50);
+    [Export]
+    public Vector2 MaxSize = new Vector2(1000, 1000);
+    public bool mouseInMove;
+    public bool mouseInScale;
     private bool isDragging;
+    private bool isScaling;
     private Vector2 dragOffset;
 
     public override void _Ready()
     {
         MoveArea.MouseEntered += () =>
         {
-            mouseIn = true;
+            mouseInMove = true;
         };
         MoveArea.MouseExited += () =>
         {
             if (!isDragging)
             {
-                mouseIn = false;
+                mouseInMove = false;
+            }
+        };
+        ScaleArea.MouseEntered += () =>
+        {
+            mouseInScale = true;
+        };
+        ScaleArea.MouseExited += () =>
+        {
+            if (!isScaling)
+            {
+                mouseInScale = false;
             }
         };
     }
 
     public override void _Process(double delta)
     {
-        if (mouseIn && Input.IsMouseButtonPressed(MouseButton.Left))
+        // Handle dragging
+        if (mouseInMove && Input.IsMouseButtonPressed(MouseButton.Left) && !isScaling)
         {
             if (!isDragging)
             {
-                // Start dragging - store the offset between mouse and panel position
+                // Start the drag
                 isDragging = true;
                 var globalMousePos = GetGlobalMousePosition();
                 dragOffset = globalMousePos - GlobalPosition;
             }
-            
-            // Update position while maintaining the offset
+            // If we are already dragging
             var newGlobalMousePos = GetGlobalMousePosition();
             GlobalPosition = newGlobalMousePos - dragOffset;
         }
@@ -44,6 +65,34 @@ public partial class MovableObject : Panel
         {
             isDragging = false;
         }
-    }
 
+        // Handle scaling
+        if (mouseInScale && Input.IsMouseButtonPressed(MouseButton.Left) && !isDragging)
+        {
+            if (!isScaling)
+            {
+                isScaling = true;
+            }
+            
+            // Get mouse position relative to this panel's position
+            var mousePos = GetLocalMousePosition();
+            var localMousePos = mousePos - GlobalPosition;
+            
+            // The new size should be where the mouse is relative to the top-left corner
+            var newSize = localMousePos;
+            
+            // Apply minimum size constraints
+            newSize.X = Mathf.Max(newSize.X, MinSize.X);
+            newSize.Y = Mathf.Max(newSize.Y, MinSize.Y);
+            // Apply maximum size constraints
+            newSize.X = Mathf.Min(newSize.X, MaxSize.X);
+            newSize.Y = Mathf.Min(newSize.Y, MaxSize.Y);
+            
+            Size = newSize;
+        }
+        else
+        {
+            isScaling = false;
+        }
+    }
 }
