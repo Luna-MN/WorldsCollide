686ca292228c409c89c86bac6118b2c00d92cf86
Matthew Nock <thematt.nock@gmail.com>
2025-10-22 18:29:35 +0100

    Minion Updates, removing logic that has been moved to minion.cs

--- Scripts/ClassScripts/Gunslinger/CapTrap.cs
+++ Scripts/ClassScripts/Gunslinger/CapTrap.cs
@@ -4,38 +4,4 @@ using System.Collections.Generic;
 
 public partial class CapTrap : Minion
 {
-    [Export]
-    public Area2D Area;
-    [Export]
-    public AnimatedSprite2D GunSpot;
-    public PrimaryWeapon wep;
-    public Character owner;
-    private List<Character> targets = new();
-    public override void _Ready()
-    {
-        Area.BodyEntered += OnTrapEnter;    
-        Area.BodyExited += OnTrapExit;
-        GunSpot.SpriteFrames = wep.SpriteFrames;
-    }
-
-    private void OnTrapEnter(Node2D body)
-    {
-        if (body is Enemy e)
-        {
-            if (e != owner && !targets.Contains(e))
-            {
-                targets.Add(e);
-            }
-        }
-    }
-    private void OnTrapExit(Node2D body)
-    {
-        if (body is Enemy e)
-        {
-            if (targets.Contains(e))
-            {
-                targets.Remove(e);
-            }
-        }
-    }
 }
