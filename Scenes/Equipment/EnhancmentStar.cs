9752c7a3dbdf5ffb47fbaf9f68da3fea825e1f36
Matthew Nock <thematt.nock@gmail.com>
2025-10-29 14:52:58 +0100

    clamp the stars,, new UI scene

--- Scenes/Equipment/EnhancmentStar.cs
+++ Scenes/Equipment/EnhancmentStar.cs
@@ -39,7 +39,12 @@ public partial class EnhancmentStar : TextureRect
     {
         if (dragging)
         {
-            GlobalPosition = GetGlobalMousePosition() - offset;
+            var newPos = GetGlobalMousePosition() - offset;
+            if (GetParent<Control>().GetRect().HasPoint(newPos))
+            {
+                GlobalPosition = newPos;
+            }
+            
         }
     }
 }
